e7d40e88a85338f362aec3b3d1633102913be0c3
Simon Tucker <simon@brotherlogic@gmail>
2020-01-27 18:02:41 -0800

    Adds ability to query records. This closes #10

--- recordbudgetutils.go
+++ recordbudgetutils.go
@@ -1,4 +1,16 @@
 package main
 
-func (s *Server) doNothing() {
+import (
+	"time"
+
+	"golang.org/x/net/context"
+)
+
+func (s *Server) rebuildBudget(ctx context.Context) (time.Time, error) {
+	_, err := s.rc.getRecordsSince(ctx, s.config.LastRecordcollectionPull)
+	if err != nil {
+		return time.Now().Add(time.Minute * 5), err
+	}
+
+	return time.Now().Add(time.Hour * 24), err
 }
