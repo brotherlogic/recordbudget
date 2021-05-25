0160ca076accf7d9d943e765d7a021a6ba75ada9
Simon Tucker <brotherlogic@gmail.com>
2021-05-25 01:15:56 -0700

    Bounce build

--- recordbudgetutils.go
+++ recordbudgetutils.go
@@ -24,7 +24,7 @@ func (s *Server) adjustDate(r *rcpb.Record) int64 {
 }
 
 func (s *Server) pullOrders(ctx context.Context, config *pb.Config) (*pb.Config, error) {
-	s.Log(fmt.Sprintf("Pulling orders from %v", config.LastOrderPull))
+	s.Log(fmt.Sprintf("Pulling orders from this time %v", config.LastOrderPull))
 
 	// Order numbers start at zero, so adjust
 	if config.LastOrderPull == 0 {
