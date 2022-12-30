2c82c3fcf13b8ee9879b12cb18d2599652b01a63
Simon Tucker <brotherlogic@gmail.com>
2022-12-30 09:32:29 -0800

    Adds in budget client

---
+++ client/client.go
@@ -0,0 +1,28 @@
+package recordbudget_client
+
+import (
+	"context"
+
+	pbgs "github.com/brotherlogic/goserver"
+	pb "github.com/brotherlogic/recordbudget/proto"
+)
+
+type RecordBudgetClient struct {
+	Gs   *pbgs.GoServer
+	Test bool
+}
+
+func (c *RecordBudgetClient) GetRecord(ctx context.Context, req *pb.GetBudgetRequest) (*pb.GetBudgetResponse, error) {
+	if c.Test {
+		return &pb.GetBudgetResponse{}, nil
+	}
+
+	conn, err := c.Gs.FDialServer(ctx, "recordbudget")
+	if err != nil {
+		return nil, err
+	}
+	defer conn.Close()
+
+	client := pb.NewRecordBudgetServiceClient(conn)
+	return client.GetBudget(ctx, req)
+}
