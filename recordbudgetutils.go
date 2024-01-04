396089c54cfe181d5a8fe2bc1955854956fb06ec
Simon Tucker <brotherlogic@gmail.com>
2024-01-04 11:37:38 -0800

    Fix increment

--- recordbudgetutils.go
+++ recordbudgetutils.go
@@ -85,7 +85,7 @@ func (s *Server) metrics(ctx context.Context, c *pb.Config) {
 			madep += float64(sold.GetPrice())
 		}
 	}
-	made.Set(madev)
+	was_parents_made.Set(madev)
 }
 
 func (s *Server) adjustDate(ctx context.Context, r *rcpb.Record) int64 {
